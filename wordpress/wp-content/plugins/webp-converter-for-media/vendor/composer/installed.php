ff56b1ef64995834ecb2895c08c37f79fb06c41c
root <root@vps.server.local>
2022-02-27 21:56:11 +0000

    commit

---
+++ wordpress/wp-content/plugins/webp-converter-for-media/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'acd63800e0c5c9781bf3bcd34110e37b053136a3',
+        'name' => 'gbiorczyk/webp-converter-for-media',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'gbiorczyk/webp-converter-for-media' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'acd63800e0c5c9781bf3bcd34110e37b053136a3',
+            'dev_requirement' => false,
+        ),
+        'matt-plugins/deactivation-modal' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../matt-plugins/deactivation-modal',
+            'aliases' => array(),
+            'reference' => 'e4382cd530992d7fb4b6e0938eda06230e18e3b5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
